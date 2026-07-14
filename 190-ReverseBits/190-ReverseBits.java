c9cf9030991d4c7b613da06f02bd15b87de873a5
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-07-14 14:11:26 +0530

    Create 190-ReverseBits

---
+++ 190-ReverseBits/190-ReverseBits.java
@@ -0,0 +1,9 @@
+// Last updated: 7/14/2026, 2:11:25 PM
+class Solution {
+    public int reverseBits(int n) {
+        StringBuilder sb = new StringBuilder(Integer.toBinaryString(n));
+        while (sb.length() < 32)
+            sb.insert(0, '0');
+        return Integer.parseInt(sb.reverse().toString(), 2);
+    }
+}
