183b8d80c52eacbf90d7e5ae746b6b4ddf9e2dc5
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-07-14 14:09:03 +0530

    Create 2416-EvaluateBooleanBinaryTree

---
+++ 2416-EvaluateBooleanBinaryTree/2416-EvaluateBooleanBinaryTree.java
@@ -0,0 +1,12 @@
+// Last updated: 7/14/2026, 2:09:02 PM
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if (root.val < 2) {
+            return root.val == 1;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        }
+        return evaluateTree(root.left) && evaluateTree(root.right);
+    }
+}
