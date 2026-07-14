6d7f2ed1eea45e295a1bf18823ef4f6282050ea4
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-07-14 14:09:15 +0530

    Create 2219-MaximumNumberOfWordsFoundInSentences

---
+++ 2219-MaximumNumberOfWordsFoundInSentences/2219-MaximumNumberOfWordsFoundInSentences.java
@@ -0,0 +1,12 @@
+// Last updated: 7/14/2026, 2:09:14 PM
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int maxLen = 0;
+    for(String currSent : sentences){
+    int currLen = currSent.split(" ").length;
+    if(maxLen<currLen)
+       maxLen = currLen;   
+    }
+    return maxLen;
+}
+}
