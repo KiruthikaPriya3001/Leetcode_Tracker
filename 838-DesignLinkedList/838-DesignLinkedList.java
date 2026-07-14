2dda42e11aaa5c071966a2590e7edb1ef96e2253
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-07-14 14:10:11 +0530

    Create 838-DesignLinkedList

---
+++ 838-DesignLinkedList/838-DesignLinkedList.java
@@ -0,0 +1,101 @@
+// Last updated: 7/14/2026, 2:10:10 PM
+class MyLinkedList {
+    private class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1; 
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data; 
+    }
+
+    public void addAtHead(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addAtTail(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) {
+            return; 
+        }
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        if (index == size) {
+            addAtTail(val);
+            return;
+        }
+        Node newNode = new Node(val);
+        Node current = head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        newNode.next = current.next;
+        current.next = newNode;
+        size++;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return; 
+        }
+        if (index == 0) {
+            head = head.next;
+            if (head == null) {
+                tail = null; 
+            }
+        } else {
+            Node current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+            if (current.next == null) {
+                tail = current; 
+            }
+        }
+        size--;
+    }
+}
