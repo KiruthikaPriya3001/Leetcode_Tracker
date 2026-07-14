7a04fea0b9cf1f5948dbbfafbd7a0da85afc6e25
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-07-14 14:10:15 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.java
@@ -0,0 +1,8 @@
+// Last updated: 7/14/2026, 2:10:14 PM
+class Solution {
+    public String toLowerCase(String s) {
+        String lowerCaseText = s.toLowerCase();
+        return lowerCaseText;
+
+    }
+}
