0b05e48da4a0ba73e770b159b61c81af79c87cc3
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-07-14 14:09:51 +0530

    Create 1329-MinimumCostToMoveChipsToTheSamePosition

---
+++ 1329-MinimumCostToMoveChipsToTheSamePosition/1329-MinimumCostToMoveChipsToTheSamePosition.java
@@ -0,0 +1,15 @@
+// Last updated: 7/14/2026, 2:09:50 PM
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int even=0;
+        int odd =0;
+        for(int pos : position){
+        if(pos % 2==0){
+            even++;
+        }else{
+            odd++;
+        }
+    }
+        return Math.min(even,odd);
+}
+}
