e9dbf43e9955eb90fe793cca165cdf057322e8e6
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-07-14 14:10:53 +0530

    Create 290-WordPattern

---
+++ 290-WordPattern/290-WordPattern.java
@@ -0,0 +1,17 @@
+// Last updated: 7/14/2026, 2:10:52 PM
+class Solution {
+  public boolean wordPattern(String pattern, String str) {
+    String[] words = str.split(" ");
+    if (words.length != pattern.length())
+      return false;
+
+    Map<Character, Integer> charToIndex = new HashMap<>();
+    Map<String, Integer> stringToIndex = new HashMap<>();
+
+    for (Integer i = 0; i < pattern.length(); ++i)
+      if (charToIndex.put(pattern.charAt(i), i) != stringToIndex.put(words[i], i))
+        return false;
+
+    return true;
+  }
+}
