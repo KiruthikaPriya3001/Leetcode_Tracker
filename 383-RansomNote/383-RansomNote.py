2510776a72d0ed36601ce050b7ba1f6a3e11ea8d
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-07-14 14:10:39 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.py
@@ -0,0 +1,9 @@
+# Last updated: 7/14/2026, 2:10:38 PM
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        st1,st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            return True
+        return False 
+
+        
