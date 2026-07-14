85b06ef82097a6b79b6ab26d0d5aeb781ac2e528
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-07-14 14:11:20 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/204-CountPrimes.java
@@ -0,0 +1,16 @@
+// Last updated: 7/14/2026, 2:11:19 PM
+class Solution {
+    public int countPrimes(int n) {
+      boolean [] notPrime = new boolean[n];
+      int count = 0;
+      for(int i = 2;i < n;i++){
+        if(notPrime[i]==false){
+            count++;
+            for(int j=2;i*j < n;j++){
+                notPrime[i*j]=true;
+            }
+        }
+      }  
+      return count;
+    }
+}
