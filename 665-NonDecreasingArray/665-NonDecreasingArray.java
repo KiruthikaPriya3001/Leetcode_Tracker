4a7aa5e884832538a15cdaae1d223a7897917656
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-07-27 11:16:02 +0530

    Create 665-NonDecreasingArray

---
+++ 665-NonDecreasingArray/665-NonDecreasingArray.java
@@ -0,0 +1,21 @@
+// Last updated: 7/27/2026, 11:16:03 AM
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int c = 0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]<nums[i-1]){
+                c++;
+
+                if(c>1)
+                return false;
+
+                if(i>=2 && nums[i-2]>nums[i]){
+                    nums[i]=nums[i-1];
+                }else{
+                    nums[i-1]=nums[i];
+                }
+            }
+        }
+        return true;
+    }
+}
