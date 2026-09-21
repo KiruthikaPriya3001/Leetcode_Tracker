b21d6381f1fde8b783a7d72deb9540dfc94c5dfb
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-09-21 14:27:52 +0530

    Create 867-TransposeMatrix

---
+++ 867-TransposeMatrix/867-TransposeMatrix.java
@@ -0,0 +1,14 @@
+// Last updated: 9/21/2026, 2:27:50 PM
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length;        
+        int n = matrix[0].length;     
+        int[][] transpose = new int[n][m]; 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+        return transpose;
+    }
+}
