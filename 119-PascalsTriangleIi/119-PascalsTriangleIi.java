85eea83feb7f53ca52b717f48db94881da6cd125
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-09-21 14:30:54 +0530

    Create 119-PascalsTriangleIi

---
+++ 119-PascalsTriangleIi/119-PascalsTriangleIi.java
@@ -0,0 +1,16 @@
+// Last updated: 9/21/2026, 2:30:52 PM
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i <= rowIndex; i++) {
+            list.add(1);
+
+            for (int j = i - 1; j > 0; j--) {
+                list.set(j, list.get(j) + list.get(j - 1));
+            }
+        }
+
+        return list;
+    }
+}
