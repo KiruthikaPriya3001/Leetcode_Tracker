729c3794e4f628fba2def4584038dd81fef98e53
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-09-21 14:23:38 +0530

    Create 3232-FindIfDigitGameCanBeWon

---
+++ 3232-FindIfDigitGameCanBeWon/3232-FindIfDigitGameCanBeWon.java
@@ -0,0 +1,20 @@
+// Last updated: 9/21/2026, 2:23:36 PM
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int suma =0;
+        int sumb =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<10){
+            suma+=nums[i];
+        }else{
+            sumb+=nums[i];
+        }
+        }
+        if(suma!=sumb)
+        {
+            return true;
+        }
+        return false;
+    }
+}
+          
