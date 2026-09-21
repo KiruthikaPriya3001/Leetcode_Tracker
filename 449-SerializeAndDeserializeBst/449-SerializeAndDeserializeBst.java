1f3195c8ab065d92e7e05c169bcd2525860183cb
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-09-21 14:29:01 +0530

    Create 449-SerializeAndDeserializeBst

---
+++ 449-SerializeAndDeserializeBst/449-SerializeAndDeserializeBst.java
@@ -0,0 +1,31 @@
+// Last updated: 9/21/2026, 2:28:59 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    static TreeNode res;
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        res = root;
+        return "";
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        return res;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// String tree = ser.serialize(root);
+// TreeNode ans = deser.deserialize(tree);
+// return ans;
