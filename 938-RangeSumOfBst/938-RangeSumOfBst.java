b2919073730cdca87851b13c608b3047060ceea3
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-09-21 14:27:19 +0530

    Create 938-RangeSumOfBst

---
+++ 938-RangeSumOfBst/938-RangeSumOfBst.java
@@ -0,0 +1,15 @@
+// Last updated: 9/21/2026, 2:27:16 PM
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) return 0;
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        } else if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        } else {
+            return root.val +
+                   rangeSumBST(root.left, low, high) +
+                   rangeSumBST(root.right, low, high);
+        }
+    }
+}
