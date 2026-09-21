4f6bf4117bdd43e9c07d88e89255c8b12135302a
Kiruthika Priya S K <kiruthikapriya3001@gmail.com>
2026-09-21 14:27:55 +0530

    Create 845-LongestMountainInArray

---
+++ 845-LongestMountainInArray/845-LongestMountainInArray.java
@@ -0,0 +1,20 @@
+// Last updated: 9/21/2026, 2:27:53 PM
+class Solution {
+    public int longestMountain(int[] arr) {
+        int ans = 0;
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                int left = i;
+                int right = i;
+                while (left > 0 && arr[left] > arr[left - 1]) {
+                    left--;
+                }
+                while (right < arr.length - 1 && arr[right] > arr[right + 1]) {
+                    right++;
+                }
+                ans = Math.max(ans, right - left + 1);
+            }
+        }
+        return ans;
+    }
+}
